71c0f08a0d90895ce2da58c48143a20990816052
senaerdogan <saadetsenaerdogan@gmail.com>
2021-11-16 19:59:11 +0300

    add training service

---
+++ training-service/src/test/java/com/trainingplatform/trainingservice/trainingservice/TrainingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trainingplatform.trainingservice.trainingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
