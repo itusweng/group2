ab7577ecad857535471e26639bbf5f5e0d4ba61d
Umut Emre Bayramoğlu <umut265@gmail.com>
2021-11-20 05:16:57 +0300

    Complete keycloak server configuration

--- api-gateway-service/src/main/java/com/trainingplatform/apigatewayservice/config/GatewayConfig.java
+++ api-gateway-service/src/main/java/com/trainingplatform/apigatewayservice/config/GatewayConfig.java
@@ -1,8 +1,6 @@
 package com.trainingplatform.apigatewayservice.config;
 
-import com.trainingplatform.apigatewayservice.filters.AuthenticationFilter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
@@ -15,17 +13,13 @@ import org.springframework.context.annotation.Configuration;
 @RequiredArgsConstructor
 public class GatewayConfig {
 
-    private final AuthenticationFilter filter;
-
     @Bean
     public RouteLocator routes(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route("streaming-service", r -> r.path("/api/streaming/**")
-                        .filters(f -> f.filter(filter))
                         .uri("lb://streaming-service"))
 
                 .route("user-service", r -> r.path("/api/user/**")
-                        .filters(f -> f.filter(filter))
                         .uri("lb://user-service"))
                 .build();
     }
---
+++ api-gateway-service/src/main/java/com/trainingplatform/apigatewayservice/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.trainingplatform.apigatewayservice.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@EnableWebFluxSecurity
+@EnableReactiveMethodSecurity
+public class SecurityConfig  {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        http
+                .authorizeExchange()
+                // User can access register and login pages without access token
+                .pathMatchers("/api/auth/*").permitAll()
+
+                // Specify api endpoints that requires authentication
+                .pathMatchers("/api/streaming/*").authenticated()
+
+                // Authentication is required for remaining endpoints
+                .anyExchange().authenticated()
+                .and()
+                .csrf().disable()
+                .oauth2Login();
+        return http.build();
+    }
+}
