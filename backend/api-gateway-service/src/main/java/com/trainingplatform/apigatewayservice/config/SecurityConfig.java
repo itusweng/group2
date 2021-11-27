e91dcac50080dc1ca6131603b6b6c8e2e2775d93
Umut Bayramoglu <umut265@gmail.com>
2021-11-28 00:19:55 +0300

    Add api gateway cors configuration

--- backend/api-gateway-service/src/main/java/com/trainingplatform/apigatewayservice/config/SecurityConfig.java
+++ backend/api-gateway-service/src/main/java/com/trainingplatform/apigatewayservice/config/SecurityConfig.java
@@ -4,6 +4,11 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @EnableWebFluxSecurity
 @EnableReactiveMethodSecurity
@@ -28,4 +33,17 @@ public class SecurityConfig  {
                 .jwt();
         return http.build();
     }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("*"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
 }
