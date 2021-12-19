874d8a825cae8e4f3251bbb8cf74e6c8b498fa7e
Umut Bayramoglu <umut265@gmail.com>
2021-12-19 16:09:29 +0300

    Update ant matchers on training service security config

--- backend/training-service/src/main/java/com/trainingplatform/trainingservice/trainingservice/config/SecurityConfig.java
+++ backend/training-service/src/main/java/com/trainingplatform/trainingservice/trainingservice/config/SecurityConfig.java
@@ -1,11 +1,10 @@
 package com.trainingplatform.trainingservice.trainingservice.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.oauth2.jwt.Jwt;
@@ -15,20 +14,8 @@ import java.util.Collection;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
-import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
-import org.springframework.security.web.SecurityFilterChain;
-
-import java.util.Collection;
 
 @Configuration
 @EnableWebSecurity
@@ -48,7 +35,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeRequests()
-                .antMatchers("/api/training/").hasAuthority("ROLE_MANAGER")
+                .antMatchers(HttpMethod.GET, "/api/training/getAllTrainings").permitAll()
+                .antMatchers(HttpMethod.POST, "/api/training/").hasAuthority("ROLE_MANAGER")
                 .anyRequest().denyAll()
                 .and()
                 .oauth2ResourceServer()
