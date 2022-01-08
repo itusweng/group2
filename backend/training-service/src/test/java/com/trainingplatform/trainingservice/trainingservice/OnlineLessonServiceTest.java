84ff47700c6499d80980c5b391b3b629d138f635
senaerdogan <saadetsenaerdogan@gmail.com>
2022-01-09 02:31:58 +0300

    create OnlineLessonService unit tests

---
+++ backend/training-service/src/test/java/com/trainingplatform/trainingservice/trainingservice/OnlineLessonServiceTest.java
@@ -0,0 +1,72 @@
+package com.trainingplatform.trainingservice.trainingservice;
+
+import com.trainingplatform.trainingservice.trainingservice.model.entity.OnlineLessonModel;
+import com.trainingplatform.trainingservice.trainingservice.model.entity.TrainingModel;
+import com.trainingplatform.trainingservice.trainingservice.model.mapper.OnlineLessonModelMapper;
+import com.trainingplatform.trainingservice.trainingservice.model.response.OnlineLessonResponseDTO;
+import com.trainingplatform.trainingservice.trainingservice.repository.OnlineLessonRepository;
+import com.trainingplatform.trainingservice.trainingservice.service.OnlineLessonService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
+public class OnlineLessonServiceTest {
+    @InjectMocks
+    OnlineLessonService onlineLessonService;
+
+    @Mock private OnlineLessonRepository onlineLessonRepo;
+    @Mock private  OnlineLessonModelMapper onlineLessonModelMapper;
+
+    @Test
+    public void it_should_return_onlinelessonresponsedtolist_when_training_found(){
+        OnlineLessonModel onlineLessonModel = mock(OnlineLessonModel.class);
+        when(onlineLessonRepo.findAllByTrainingID(1L)).thenReturn(List.of(onlineLessonModel));
+        List<OnlineLessonResponseDTO> onlineLessonResponseDTOS = new ArrayList<>();
+        OnlineLessonResponseDTO onlineLessonResponseDTO = mock(OnlineLessonResponseDTO.class);
+        when(onlineLessonModelMapper.mapToDto(onlineLessonModel)).thenReturn(onlineLessonResponseDTO);
+        onlineLessonResponseDTOS.add(onlineLessonResponseDTO);
+        //act
+        List<OnlineLessonResponseDTO> onlineLessonResponseDTOS1 = onlineLessonService.getOnlineLessons(1L);
+        //assert
+        assertThat(onlineLessonResponseDTOS).isEqualTo(onlineLessonResponseDTOS1);
+    }
+
+    @Test
+    public void it_should_return_onlinelessonresponsedto_when_lesson_created(){
+        //arrange
+        OnlineLessonModel onlineLessonModel = mock(OnlineLessonModel.class);
+        OnlineLessonModel savedOnlineLessonModel = mock(OnlineLessonModel.class);
+        when(onlineLessonRepo.save(onlineLessonModel)).thenReturn(savedOnlineLessonModel);
+        TrainingModel trainingModel = mock(TrainingModel.class);
+        when(savedOnlineLessonModel.getTraining()).thenReturn(trainingModel);
+        when(savedOnlineLessonModel.getTraining().getId()).thenReturn(1L);
+        OnlineLessonResponseDTO onlineLessonResponseDTO = mock(OnlineLessonResponseDTO.class);
+        when(onlineLessonModelMapper.mapToDto(savedOnlineLessonModel)).thenReturn(onlineLessonResponseDTO);
+        onlineLessonResponseDTO.setTraining_id(savedOnlineLessonModel.getTraining().getId());
+        //act
+        OnlineLessonResponseDTO onlineLessonResponseDTO1 = onlineLessonService.createOnlineLesson(onlineLessonModel);
+        //assert
+        assertThat(onlineLessonResponseDTO).isEqualTo(onlineLessonResponseDTO1);
+    }
+
+    @Test
+    public void it_should_delete_lesson_when_lesson_found(){
+        //arrange
+        //act
+        onlineLessonService.deleteOnlineLesson(1L);
+        //arrange
+        verify(onlineLessonRepo, times(1)).deleteById(any(Long.class));
+    }
+}
