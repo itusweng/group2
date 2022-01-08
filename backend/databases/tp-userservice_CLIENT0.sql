c0545dc41b1cc69fb3d8acd776f6ea21d06b48fa
Umut Bayramoglu <umut265@gmail.com>
2022-01-08 22:29:02 +0300

    Update database tables

---
+++ backend/databases/tp-userservice_CLIENT0.sql
@@ -0,0 +1,77 @@
+-- MySQL dump 10.13  Distrib 8.0.27, for macos11 (x86_64)
+--
+-- Host: localhost    Database: tp-userservice
+-- ------------------------------------------------------
+-- Server version	8.0.27
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `CLIENT`
+--
+
+DROP TABLE IF EXISTS `CLIENT`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `CLIENT` (
+  `ID` varchar(36) NOT NULL,
+  `ENABLED` bit(1) NOT NULL DEFAULT b'0',
+  `FULL_SCOPE_ALLOWED` bit(1) NOT NULL DEFAULT b'0',
+  `CLIENT_ID` varchar(255) DEFAULT NULL,
+  `NOT_BEFORE` int DEFAULT NULL,
+  `PUBLIC_CLIENT` bit(1) NOT NULL DEFAULT b'0',
+  `SECRET` varchar(255) DEFAULT NULL,
+  `BASE_URL` varchar(255) DEFAULT NULL,
+  `BEARER_ONLY` bit(1) NOT NULL DEFAULT b'0',
+  `MANAGEMENT_URL` varchar(255) DEFAULT NULL,
+  `SURROGATE_AUTH_REQUIRED` bit(1) NOT NULL DEFAULT b'0',
+  `REALM_ID` varchar(36) DEFAULT NULL,
+  `PROTOCOL` varchar(255) DEFAULT NULL,
+  `NODE_REREG_TIMEOUT` int DEFAULT '0',
+  `FRONTCHANNEL_LOGOUT` bit(1) NOT NULL DEFAULT b'0',
+  `CONSENT_REQUIRED` bit(1) NOT NULL DEFAULT b'0',
+  `NAME` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `SERVICE_ACCOUNTS_ENABLED` bit(1) NOT NULL DEFAULT b'0',
+  `CLIENT_AUTHENTICATOR_TYPE` varchar(255) DEFAULT NULL,
+  `ROOT_URL` varchar(255) DEFAULT NULL,
+  `DESCRIPTION` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `REGISTRATION_TOKEN` varchar(255) DEFAULT NULL,
+  `STANDARD_FLOW_ENABLED` bit(1) NOT NULL DEFAULT b'1',
+  `IMPLICIT_FLOW_ENABLED` bit(1) NOT NULL DEFAULT b'0',
+  `DIRECT_ACCESS_GRANTS_ENABLED` bit(1) NOT NULL DEFAULT b'0',
+  `ALWAYS_DISPLAY_IN_CONSOLE` bit(1) NOT NULL DEFAULT b'0',
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `UK_B71CJLBENV945RB6GCON438AT` (`REALM_ID`,`CLIENT_ID`),
+  KEY `IDX_CLIENT_ID` (`CLIENT_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `CLIENT`
+--
+
+LOCK TABLES `CLIENT` WRITE;
+/*!40000 ALTER TABLE `CLIENT` DISABLE KEYS */;
+INSERT INTO `CLIENT` VALUES ('014dd004-06d5-49dd-a89a-293f7c5974a2',_binary '',_binary '\0','admin-cli',0,_binary '',NULL,NULL,_binary '\0',NULL,_binary '\0','master','openid-connect',0,_binary '\0',_binary '\0','${client_admin-cli}',_binary '\0','client-secret',NULL,NULL,NULL,_binary '\0',_binary '\0',_binary '',_binary '\0'),('1dbb1163-f4c3-44ae-a385-46d76862cd86',_binary '',_binary '\0','security-admin-console',0,_binary '',NULL,'/admin/training-platform-realm/console/',_binary '\0',NULL,_binary '\0','training-platform-realm','openid-connect',0,_binary '\0',_binary '\0','${client_security-admin-console}',_binary '\0','client-secret','${authAdminUrl}',NULL,NULL,_binary '',_binary '\0',_binary '\0',_binary '\0'),('22c20439-6887-43b5-813b-49f191fadb40',_binary '',_binary '','tp-spring-cloud-gateway-client',0,_binary '\0','38e79dcd-942b-448e-a7cc-07e503b06e59','http://localhost:8081/',_binary '\0','http://localhost:8081/',_binary '\0','training-platform-realm','openid-connect',-1,_binary '\0',_binary '\0',NULL,_binary '','client-secret','',NULL,NULL,_binary '',_binary '\0',_binary '',_binary '\0'),('2493d4c4-276d-4a6b-99af-09b9563d031f',_binary '',_binary '\0','broker',0,_binary '\0',NULL,NULL,_binary '',NULL,_binary '\0','master','openid-connect',0,_binary '\0',_binary '\0','${client_broker}',_binary '\0','client-secret',NULL,NULL,NULL,_binary '',_binary '\0',_binary '\0',_binary '\0'),('3694ae68-0198-42f4-8280-bade470681e6',_binary '',_binary '\0','account-console',0,_binary '',NULL,'/realms/master/account/',_binary '\0',NULL,_binary '\0','master','openid-connect',0,_binary '\0',_binary '\0','${client_account-console}',_binary '\0','client-secret','${authBaseUrl}',NULL,NULL,_binary '',_binary '\0',_binary '\0',_binary '\0'),('412bc1ee-d68f-4be5-96bf-cd7fe022e19d',_binary '',_binary '\0','master-realm',0,_binary '\0',NULL,NULL,_binary '',NULL,_binary '\0','master',NULL,0,_binary '\0',_binary '\0','master Realm',_binary '\0','client-secret',NULL,NULL,NULL,_binary '',_binary '\0',_binary '\0',_binary '\0'),('5c672d07-da67-4494-b773-71c6df07837c',_binary '',_binary '\0','realm-management',0,_binary '\0',NULL,NULL,_binary '',NULL,_binary '\0','training-platform-realm','openid-connect',0,_binary '\0',_binary '\0','${client_realm-management}',_binary '\0','client-secret',NULL,NULL,NULL,_binary '',_binary '\0',_binary '\0',_binary '\0'),('6fba9dd5-348e-4bda-b418-58aa1a5a1555',_binary '',_binary '\0','security-admin-console',0,_binary '',NULL,'/admin/master/console/',_binary '\0',NULL,_binary '\0','master','openid-connect',0,_binary '\0',_binary '\0','${client_security-admin-console}',_binary '\0','client-secret','${authAdminUrl}',NULL,NULL,_binary '',_binary '\0',_binary '\0',_binary '\0'),('75a57c7d-19a9-49f3-8796-778f11f5552d',_binary '',_binary '\0','account',0,_binary '',NULL,'/realms/training-platform-realm/account/',_binary '\0',NULL,_binary '\0','training-platform-realm','openid-connect',0,_binary '\0',_binary '\0','${client_account}',_binary '\0','client-secret','${authBaseUrl}',NULL,NULL,_binary '',_binary '\0',_binary '\0',_binary '\0'),('79915969-616f-45bf-85bf-615bac5c29b2',_binary '',_binary '\0','account',0,_binary '',NULL,'/realms/master/account/',_binary '\0',NULL,_binary '\0','master','openid-connect',0,_binary '\0',_binary '\0','${client_account}',_binary '\0','client-secret','${authBaseUrl}',NULL,NULL,_binary '',_binary '\0',_binary '\0',_binary '\0'),('a8eee0ec-1798-424e-b2b2-9181f32eda68',_binary '',_binary '\0','admin-cli',0,_binary '',NULL,NULL,_binary '\0',NULL,_binary '\0','training-platform-realm','openid-connect',0,_binary '\0',_binary '\0','${client_admin-cli}',_binary '\0','client-secret',NULL,NULL,NULL,_binary '\0',_binary '\0',_binary '',_binary '\0'),('bd783b6e-4231-45a8-a878-71215870684f',_binary '',_binary '\0','training-platform-realm-realm',0,_binary '\0',NULL,NULL,_binary '',NULL,_binary '\0','master',NULL,0,_binary '\0',_binary '\0','training-platform-realm Realm',_binary '\0','client-secret',NULL,NULL,NULL,_binary '',_binary '\0',_binary '\0',_binary '\0'),('bf730706-6231-4f83-ac31-aaf9646a826e',_binary '',_binary '\0','account-console',0,_binary '',NULL,'/realms/training-platform-realm/account/',_binary '\0',NULL,_binary '\0','training-platform-realm','openid-connect',0,_binary '\0',_binary '\0','${client_account-console}',_binary '\0','client-secret','${authBaseUrl}',NULL,NULL,_binary '',_binary '\0',_binary '\0',_binary '\0'),('c5a95f7a-298c-4a2e-9150-3e3cde3788e2',_binary '',_binary '\0','broker',0,_binary '\0',NULL,NULL,_binary '',NULL,_binary '\0','training-platform-realm','openid-connect',0,_binary '\0',_binary '\0','${client_broker}',_binary '\0','client-secret',NULL,NULL,NULL,_binary '',_binary '\0',_binary '\0',_binary '\0');
+/*!40000 ALTER TABLE `CLIENT` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-01-08 22:27:34
