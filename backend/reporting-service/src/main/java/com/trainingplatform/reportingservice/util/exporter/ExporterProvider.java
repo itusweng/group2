6df5b56d39f9617e4e5ef613c0ca11c3f3f38cc6
Umut Bayramoglu <umut265@gmail.com>
2022-01-09 01:10:20 +0300

    Upload maven files

---
+++ backend/reporting-service/src/main/java/com/trainingplatform/reportingservice/util/exporter/ExporterProvider.java
@@ -0,0 +1,17 @@
+package com.trainingplatform.reportingservice.util.exporter;
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+public class ExporterProvider implements IExporter {
+
+    @Override
+    public void setResponse(HttpServletResponse response, String fileName) {
+        response.setContentType("application/pdf");
+        String headerKey = "Content-Disposition";
+        String headerValue = String.format("attachment; filename=%s", fileName);
+        response.setHeader(headerKey, headerValue);
+    }
+}
---
+++ backend/reporting-service/src/test/java/com/trainingplatform/reportingservice/ReportingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trainingplatform.reportingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
