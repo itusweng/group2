a7e5164955aeec7b6ef43f4b3e81456725f559b9
Ertuğrul BEKTİK <ertugrulbektik99@gmail.com>
2022-01-09 17:39:45 +0300

    User list updated

--- frontend/src/view/pages/admin/users/UserList.vue
+++ frontend/src/view/pages/admin/users/UserList.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
     <b-alert
-        show
-        variant="light"
-        class="alert alert-custom alert-white alert-shadow fade show gutter-b"
+      show
+      variant="light"
+      class="alert alert-custom alert-white alert-shadow fade show gutter-b"
     >
       <div class="alert-icon">
         <span class="svg-icon svg-icon-lg">
@@ -25,37 +25,47 @@
       </b-row>
       <b-row class="mt-5">
         <b-col md="12">
-          <b-table :fields="tableFields" :items="users">
+          <b-table :fields="tableFields" :items="users" :busy="loading">
             <template v-slot:cell(actions)="{ item }">
               <b-button
-                  :to="'/admin/users/' + item.id + '/details'"
-                  class="btn btn-icon btn-light btn-hover-primary btn-sm"
-                  v-b-tooltip="'Details'"
+                :to="'/admin/users/' + item.id + '/details'"
+                class="btn btn-icon btn-light btn-hover-primary btn-sm"
+                v-b-tooltip="'Details'"
               >
                 <span class="svg-icon svg-icon-md svg-icon-primary">
                   <inline-svg src="/media/svg/icons/General/Settings-1.svg" />
                 </span>
               </b-button>
               <b-button
-                  :to="'/admin/users/' + item.id + '/update'"
-                  class="btn btn-icon btn-light btn-hover-primary btn-sm mx-3"
-                  v-b-tooltip="'Edit'"
+                :to="'/admin/users/' + item.id + '/update'"
+                class="btn btn-icon btn-light btn-hover-primary btn-sm mx-3"
+                v-b-tooltip="'Edit'"
               >
                 <span class="svg-icon svg-icon-md svg-icon-primary">
                   <inline-svg src="/media/svg/icons/Communication/Write.svg" />
                 </span>
               </b-button>
               <b-button
-                  class="btn btn-icon btn-light btn-hover-primary btn-sm"
-                  v-b-tooltip="'Delete'"
-                  @click="deleteUser(item)"
+                class="btn btn-icon btn-light btn-hover-primary btn-sm"
+                v-b-tooltip="'Delete'"
+                @click="deleteUser(item)"
               >
                 <span class="svg-icon svg-icon-md svg-icon-primary">
                   <inline-svg src="/media/svg/icons/General/Trash.svg" />
                 </span>
               </b-button>
             </template>
+            <template v-slot:table-busy>
+              <div class="text-center">
+                <b-spinner variant="primary" />
+              </div>
+            </template>
           </b-table>
+          <pagination
+            :total-rows="total"
+            :per-page.sync="pagination.size"
+            :current-page.sync="pagination.page"
+          />
         </b-col>
       </b-row>
     </card>
@@ -66,48 +76,63 @@
 export default {
   data() {
     return {
-      users: [
-        {
-          id: 'f6b706eb-5e12-4c01-8821-155452239a21',
-          firstName: 'Umut',
-          lastName: 'Oğuz',
-          email: 'umutoguz@sirket.com'
-        },
-        {
-          id: 'b36bd5ad-7dad-48d6-98a8-39db9ea37abc',
-          firstName: 'Sena',
-          lastName: 'Yıldırım',
-          email: 'senayildirim@sirket.com'
-        },
-        {
-          id: '3d5de70c-0b5b-420b-886e-ebfbf9be41eb',
-          firstName: 'Ertuğrul',
-          lastName: 'Yılmaz',
-          emailAddress: 'ertugrulyilmaz@sirket.com'
-        }
-      ],
+      users: [],
       tableFields: [
         {
-          key: 'firstName',
-          sortable: true
-        },{
-          key: 'lastName',
-          sortable: true
+          key: 'first_name'
         },
         {
-          key: 'emailAddress',
-          sortable: true
+          key: 'last_name'
+        },
+        {
+          key: 'email'
+        },
+        {
+          key: 'role_name'
         },
         {
           key: 'actions'
         }
-      ]
+      ],
+      loading: false,
+      total: 0,
+      pagination: {
+        page: 1,
+        size: 20
+      }
     };
   },
+
+  created() {
+    this.getUsers();
+  },
   methods: {
+    async getUsers() {
+      try {
+        this.loading = true;
+        const { data } = await this.axios.get('/user/getAllUsers', {
+          params: {
+            page: this.pagination.page - 1,
+            size: this.pagination.size
+          }
+        });
+        this.users = data.data.users;
+        this.total = data.data.total;
+      } catch (e) {
+        console.log(e);
+      } finally {
+        this.loading = false;
+      }
+    },
     deleteUser(user) {
       this.confirmDelete();
       console.log(user);
+    }
+  },
+  watch: {
+    pagination: {
+      handler: 'getUsers',
+      deep: true
     }
   }
 };
