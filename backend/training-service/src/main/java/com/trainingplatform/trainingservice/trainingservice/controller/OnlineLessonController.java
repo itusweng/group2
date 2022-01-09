df6ce5af08d46f2c8ec47957fba57a6cd857a794
Umut Bayramoglu <umut265@gmail.com>
2022-01-09 17:13:10 +0300

    Refactor 'create online lesson' controller

--- backend/training-service/src/main/java/com/trainingplatform/trainingservice/trainingservice/controller/OnlineLessonController.java
+++ backend/training-service/src/main/java/com/trainingplatform/trainingservice/trainingservice/controller/OnlineLessonController.java
@@ -4,9 +4,8 @@ import com.trainingplatform.trainingservice.trainingservice.model.entity.OnlineL
 import com.trainingplatform.trainingservice.trainingservice.model.mapper.OnlineLessonModelMapper;
 import com.trainingplatform.trainingservice.trainingservice.model.request.offlinelesson.OnlineLessonRequestDTO;
 import com.trainingplatform.trainingservice.trainingservice.model.response.onlinelesson.OnlineLessonResponseDTO;
-import com.trainingplatform.trainingservice.trainingservice.repository.OnlineLessonRepository;
-import com.trainingplatform.trainingservice.trainingservice.repository.TrainingRepository;
 import com.trainingplatform.trainingservice.trainingservice.service.OnlineLessonService;
+import com.trainingplatform.trainingservice.trainingservice.service.TrainingService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,10 +16,11 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/training/onlineLesson")
-public class OnlineLessonController extends BaseController{
+public class OnlineLessonController extends BaseController {
+
+    private final TrainingService trainingService;
     private final OnlineLessonService onlineLessonService;
     private final OnlineLessonModelMapper onlineLessonModelMapper;
-    private final TrainingRepository trainingRepo;
 
     @GetMapping("/getAllLessons/{trainingId}")
     public ResponseEntity<HashMap<String, Object>> getAllOnlineLessons(@PathVariable Long trainingId) {
@@ -45,10 +45,9 @@ public class OnlineLessonController extends BaseController{
 
     @PostMapping("/")
     public ResponseEntity<HashMap<String, Object>> createOnlineLesson(@RequestBody OnlineLessonRequestDTO onlineLessonRequestDTO) {
-        OnlineLessonModel onlineLesson = onlineLessonModelMapper.mapToEntity(onlineLessonRequestDTO);
-        onlineLesson.setTraining(trainingRepo.getById(onlineLessonRequestDTO.getTraining_id()));
         try {
-            // TODO: test error cases
+            OnlineLessonModel onlineLesson = onlineLessonModelMapper.mapToEntity(onlineLessonRequestDTO);
+            onlineLesson.setTraining(trainingService.getTrainingById(onlineLessonRequestDTO.getTraining_id()));
             OnlineLessonResponseDTO new_onlineLesson = onlineLessonService.createOnlineLesson(onlineLesson);
             return ResponseEntity.ok(createReturnObj("Online Lesson created successfully!", new_onlineLesson));
         } catch (Exception e) {
@@ -57,16 +56,15 @@ public class OnlineLessonController extends BaseController{
     }
 
     @PostMapping("/update/{onlineLessonId}")
-    public ResponseEntity<HashMap<String, Object>> updateOnlineLesson(@PathVariable Long onlineLessonId, @RequestBody OnlineLessonRequestDTO onlineLesson){
-        try{
+    public ResponseEntity<HashMap<String, Object>> updateOnlineLesson(@PathVariable Long onlineLessonId, @RequestBody OnlineLessonRequestDTO onlineLesson) {
+        try {
             OnlineLessonModel onlineLessonModel = onlineLessonModelMapper.mapToEntity(onlineLesson);
             onlineLessonModel.setId(onlineLessonId);
 
             onlineLessonService.updateOnlineLesson(onlineLessonModel);
             return ResponseEntity.ok(
                     createReturnObj(String.format("Offline lesson updated by id successfully!", onlineLessonId), null));
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             return exceptionHandler(e);
         }
     }
